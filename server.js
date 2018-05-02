e936bbd370817bdf5358b7b94e0be74f0613c4d6
Timothy Smith <tim@shopstream.io>
2018-05-02 08:39:07 -0600

    adding heroku stuff

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
-const PORT = 8080;
+var express = require('express');
+var app = express();
+var port=Number(process.env.PORT || 3000);
 
 app.use('/', express.static(`${__dirname}/public/`));
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
