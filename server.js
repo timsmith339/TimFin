fd8d05cb6bbd824ad0793c10a13375365146e70e
Timothy Smith <tim@shopstream.io>
2018-05-02 08:44:58 -0600

    fixing server.js directory

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port=Number(process.env.PORT || 3000);
 
-app.use('/', express.static(`${__dirname}/public/`));
+app.use('/', express.static(`${__dirname}/`));
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
